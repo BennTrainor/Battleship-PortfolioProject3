50be7cb57ef37259175f38faff345627eb226839
BennTrainor <>
2023-06-15 07:22:37 +0000

    create and print game board

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""
+function to create gameboard
+"""
+def create_board():
+    return [['0' for _ in range(5)] for _ in range(5)]
+
+"""
+function to print gameboard
+"""
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
+
+board = create_board()
+print_board(board)
+
+
